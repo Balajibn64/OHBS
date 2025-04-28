38e6ed19a241fecb8d35a62d8a818525e879c924
balajibn64 <balajibn6464.com>
2025-04-28 07:51:50 +0530

    Initial commit

---
+++ src/main/java/com/ohbs/OhbsApplication.java
@@ -0,0 +1,13 @@
+package com.ohbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohbs/OhbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
