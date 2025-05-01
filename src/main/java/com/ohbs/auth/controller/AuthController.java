1a0ea8be25e72b0597d9dfb3fe9ddc6d00f91915
balajibn64 <balajibn6464.com>
2025-05-01 23:34:37 +0530

    Allowed CrossOrgins Requests

--- src/main/java/com/ohbs/auth/controller/AuthController.java
+++ src/main/java/com/ohbs/auth/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import com.ohbs.auth.service.AuthService;
 import jakarta.validation.Valid;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/auth")
 public class AuthController {
 
