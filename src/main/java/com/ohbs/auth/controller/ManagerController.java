f0b84a3259c2021859609f0b4aa967e39b210cf0
Balaji N <147259911+Balajibn64@users.noreply.github.com>
2025-05-01 23:56:19 +0530

    Balaji (#3)
    * Updated Code of Login & Register
    
    * Resolved merge conflicts and updated pom.xml
    
    * Allowed CrossOrgins Requests
    
    ---------
    
    Co-authored-by: balajibn64 <balajibn6464.com>

--- src/main/java/com/ohbs/auth/controller/ManagerController.java
+++ src/main/java/com/ohbs/auth/controller/ManagerController.java
@@ -1,11 +1,13 @@
 package com.ohbs.auth.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/manager")
 public class ManagerController {
 
