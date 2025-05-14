52e58a7081ddb5595a721f51ac496f2bc9085ae8
Jeevanantham-Jc <cjeevanantham9563@gmail.com>
2025-05-14 22:04:59 +0530

    commit

--- src/main/java/com/ohbs/manager/dto/ManagerRequestDTO.java
+++ src/main/java/com/ohbs/manager/dto/ManagerRequestDTO.java
@@ -1,17 +1,22 @@
 package com.ohbs.manager.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class ManagerRequestDTO {
-    @NotBlank
+
+    @NotBlank(message = "First name is mandatory")
     private String firstName;
 
-    @NotBlank
+    @NotBlank(message = "Last name is mandatory")
     private String lastName;
 
+    @Pattern(regexp = "^\\d{10}$", message = "Phone number must be 10 digits")
     private String phone;
 
+    @NotNull(message = "Hotel ID is required")
     private Long hotelId;
 }
